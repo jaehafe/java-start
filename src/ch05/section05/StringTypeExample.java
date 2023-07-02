07ef7655e2484b5edd6a26f418709a60e4e17dfb
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-03 06:22:45 +0900

    feat: split, string type

---
+++ src/ch05/section05/StringTypeExample.java
@@ -0,0 +1,11 @@
+package ch05.section05;
+
+public class StringTypeExample {
+    public static void main(String[] args) {
+        String subject = "자바 프로그래밍";
+        int index = subject.indexOf("프로그래밍");
+
+        System.out.println(index);
+
+    }
+}

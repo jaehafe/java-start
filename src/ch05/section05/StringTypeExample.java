59353cafb0d0d5b149f5823de051c6f0ff038ded
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-03 06:50:24 +0900

    feat: Array Type

--- src/ch05/section05/StringTypeExample.java
+++ src/ch05/section05/StringTypeExample.java
@@ -1,11 +1,12 @@
 package ch05.section05;
 
+import java.util.Arrays;
+
 public class StringTypeExample {
     public static void main(String[] args) {
         String subject = "자바 프로그래밍";
         int index = subject.indexOf("프로그래밍");
 
         System.out.println(index);
-
     }
 }

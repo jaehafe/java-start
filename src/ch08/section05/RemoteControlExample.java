75029aa3f508aa389171d5c361058efdeaf53866
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-04 12:15:00 +0900

    feat: interface - static method

---
+++ src/ch08/section05/RemoteControlExample.java
@@ -0,0 +1,25 @@
+package ch08.section05;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl rc;
+
+        rc = new Television();
+        rc.turnOn();
+        rc.setVolume(5);
+        rc.turnOff();
+
+        rc.setMute(true);
+        rc.setMute(false);
+        System.out.println("current volume: " + rc.getVolume());
+
+        rc = new Audio();
+        rc.turnOn();
+        rc.setVolume(5);
+
+        rc.setMute(true);
+        rc.setMute(false);
+        System.out.println("current volume: " + rc.getVolume());
+
+    }
+}

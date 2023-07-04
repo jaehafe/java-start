aa8c93ac6c74a0759ccc17634555d5e3648efce8
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-04 11:28:42 +0900

    feat: implements interface

---
+++ src/ch08/section02/Audio.java
@@ -0,0 +1,9 @@
+package ch08.section02;
+
+public class Audio implements RemoteControl {
+    @Override
+    public void turnOn() {
+        System.out.println("turn on Audio");
+    }
+
+}
---
+++ src/ch08/section02/RemoteControl.java
@@ -0,0 +1,6 @@
+package ch08.section02;
+
+public interface RemoteControl {
+    // public 추상 메서드
+    public void turnOn();
+}

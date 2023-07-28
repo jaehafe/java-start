af08d7055c30f605c4836c30c7b4e63dbab4b4f9
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-28 21:18:24 +0900

    feat: 다형성

---
+++ src/ch08/section11/exam01/KumhoTire.java
@@ -0,0 +1,8 @@
+package ch08.section11.exam01;
+
+public class KumhoTire implements Tire{
+    @Override
+    public void roll() {
+        System.out.println("KumhoTire is moving");
+    }
+}
---
+++ src/ch08/section11/exam01/Tire.java
@@ -0,0 +1,5 @@
+package ch08.section11.exam01;
+
+public interface Tire {
+    void roll();
+}

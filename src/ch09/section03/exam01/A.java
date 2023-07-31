0a11afc309306508465d67118d6378bbed64e648
Jaeha Lee <wogkdkrm112@gmail.com>
2023-08-01 01:33:51 +0900

    feat: 9.3 정적 멤버 클래스

---
+++ src/ch09/section03/exam01/A.java
@@ -0,0 +1,24 @@
+package ch09.section03.exam01;
+
+public class A {
+    // static이 있으면 A객체가 없어도 B를 사용 사능
+    static class B {
+        void methodB() {
+            System.out.println("methodB() 실행");
+        }
+    }
+
+    static B field2 = new B();
+
+    A() {
+        B b = new B();
+    }
+
+    void method() {
+        B b = new B();
+    }
+
+    static void method2() {
+        B b = new B();
+    }
+}
---
+++ src/ch09/section03/exam01/AExample.java
@@ -0,0 +1,8 @@
+package ch09.section03.exam01;
+
+public class AExample {
+    public static void main(String[] args) {
+        A.B b = new A.B();
+        b.methodB();
+    }
+}

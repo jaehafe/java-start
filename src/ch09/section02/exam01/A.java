6c3d4b6ef8e071eddade04a424e6f59980f66288
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-31 21:46:16 +0900

    feat: 9.2 instance member class

---
+++ src/ch09/section02/exam01/A.java
@@ -0,0 +1,23 @@
+package ch09.section02.exam01;
+
+public class A {
+    // 인스턴스 멤버 클래스
+    class B {
+        public void methodB() {
+            System.out.println("methodB() 실행");
+        }
+    }
+
+    // B 객체 대입
+    B field = new B();
+
+    // 생성자
+    A() {
+        B b = new B();
+        b.methodB();
+    }
+
+    void method() {
+        B b = new B();
+    }
+}

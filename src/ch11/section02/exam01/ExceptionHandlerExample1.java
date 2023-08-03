54f2dfbd1562ec90fd90d264358b593d92c3ee68
Jaeha Lee <wogkdkrm112@gmail.com>
2023-08-03 14:03:18 +0900

    feat: 11.2 exam02 ClassNotFoundException

--- src/ch11/section02/exam01/ExceptionHandlerExample1.java
+++ src/ch11/section02/exam01/ExceptionHandlerExample1.java
@@ -6,7 +6,10 @@ public class ExceptionHandlerExample1 {
             int result = data.length();
             System.out.println("문자 수: " + result);
         } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
+//            System.out.println(e.getMessage());
+//            System.out.println(e.toString());
+//            e.printStackTrace();
+
         } finally {
             System.out.println("마무리 실행");
         }

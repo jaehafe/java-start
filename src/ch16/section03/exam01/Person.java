45d484e5d6cda0723d7796cc7549a6dc169cc23d
Jaeha Lee <wogkdkrm112@gmail.com>
2023-08-05 10:32:16 +0900

    feat: 16.3 매개변수가 있는 람다식

---
+++ src/ch16/section03/exam01/Person.java
@@ -0,0 +1,11 @@
+package ch16.section03.exam01;
+
+public class Person {
+    public void action1(Workable workable) {
+        workable.work("Adam", "programming");
+    }
+
+    public void action2(Speakable speakable) {
+        speakable.speak("Hello");
+    }
+}
---
+++ src/ch16/section03/exam01/Speakable.java
@@ -0,0 +1,6 @@
+package ch16.section03.exam01;
+
+@FunctionalInterface
+public interface Speakable {
+    void speak(String content);
+}

41db1a78b66ce7f8576b4d553a115468d9905318
Jaeha Lee <wogkdkrm112@gmail.com>
2023-08-06 10:53:41 +0900

    chore: minor update

--- src/ch16/section05/exam03/ConstructReferenceExample.java
+++ src/ch16/section05/exam03/ConstructReferenceExample.java
@@ -4,7 +4,9 @@ public class ConstructReferenceExample {
     public static void main(String[] args) {
         Person person = new Person();
 
-        Member m1 = person.getMember1(Member::new);
+//        Member m1 = person.getMember1(Member::new);
+        Member m1 = person.getMember1((id) -> new Member(id));
+
         System.out.println(m1);
 
         System.out.println("-----------");
